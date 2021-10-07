d5366591f0b85cb5811049b386fd152f3bb5e1ea
yerden <Ytolik167349>
2021-10-07 22:26:52 +0600

    init project

---
+++ Assets/Scripts/Ads.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+using System.Collections;
+
+public class Ads : MonoBehaviour {
+
+    private Coroutine showAd;
+
+    private string gameId = "3587398", type = "video";
+    private bool testMode = true, needToStop;
+
+    private static int countLoses;
+
+    private void Start() {
+        Advertisement.Initialize(gameId, testMode);
+
+        countLoses++;
+        if(countLoses %3 == 0)
+            showAd = StartCoroutine(ShowAd());
+    }
+
+    private void Update() {
+        if(needToStop) {
+            needToStop = false;
+            StopCoroutine(showAd);
+        }
+    }
+
+    IEnumerator ShowAd() {
+        while(true) {
+            if(Advertisement.IsReady(type)) {
+                Advertisement.Show(type);
+                needToStop = true;
+            }
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+}
---
+++ Assets/Scripts/CanvasButtons.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CanvasButtons : MonoBehaviour {
+
+    public Sprite musicOn, musicOff;
+
+    private void Start() {
+        if (PlayerPrefs.GetString("music") == "No" && gameObject.name == "Music")
+            GetComponent<Image>().sprite = musicOff;
+    }
+
+    public void RestartGame() {
+        if (PlayerPrefs.GetString("music") != "No")
+            GetComponent<AudioSource>().Play();
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadInstagram() {
+        if (PlayerPrefs.GetString("music") != "No")
+            GetComponent<AudioSource>().Play();
+
+        Application.OpenURL("https://www.instagram.com/itproger_official/");
+    }
+
+    public void LoadShop() {
+        if (PlayerPrefs.GetString("music") != "No")
+            GetComponent<AudioSource>().Play();
+
+        SceneManager.LoadScene("Shop");
+    }
+
+    public void CloseShop() {
+        if (PlayerPrefs.GetString("music") != "No")
+            GetComponent<AudioSource>().Play();
+
+        SceneManager.LoadScene("Main");
+    }
+
+    public void MusicWork() {
+        // Сейчас музыка выключена и её нужно включить
+        if(PlayerPrefs.GetString("music") == "No") {
+            GetComponent<AudioSource>().Play();
+            PlayerPrefs.SetString("music", "Yes");
+            GetComponent<Image>().sprite = musicOn;
+        } else {
+            PlayerPrefs.SetString("music", "No");
+            GetComponent<Image>().sprite = musicOff;
+        }
+    }
+
+}
